092edfda6abc24bf3f7594b380d24144184a518b
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2015-04-05 13:37:05 -0500

    add files
    Files for POC2 1st project: zombies

---
+++ poc_zombie.py
@@ -0,0 +1,124 @@
+__author__ = 'jeredyang'
+
+"""
+Student portion of Zombie Apocalypse mini-project
+"""
+
+import random
+import poc_grid
+import poc_queue
+# import poc_zombie_gui
+
+# global constants
+EMPTY = 0
+FULL = 1
+FOUR_WAY = 0
+EIGHT_WAY = 1
+OBSTACLE = "obstacle"
+HUMAN = "human"
+ZOMBIE = "zombie"
+
+
+class Zombie(poc_grid.Grid):
+    """
+    Class for simulating zombie pursuit of human on grid with
+    obstacles
+    """
+
+    def __init__(self, grid_height, grid_width, obstacle_list = None,
+                 zombie_list = None, human_list = None):
+        """
+        Create a simulation of given size with given obstacles,
+        humans, and zombies
+        """
+        poc_grid.Grid.__init__(self, grid_height, grid_width)
+        if obstacle_list is not None:
+            for cell in obstacle_list:
+                self.set_full(cell[0], cell[1])
+        if zombie_list is not None:
+            self._zombie_list = list(zombie_list)
+        else:
+            self._zombie_list = []
+        if human_list is not None:
+            self._human_list = list(human_list)
+        else:
+            self._human_list = []
+
+    def clear(self):
+        """
+        Set cells in obstacle grid to be empty
+        Reset zombie and human lists to be empty
+        """
+        # clear obstacle
+        self.clear()
+
+        # clear human and zombie list
+        self._zombie_list = []
+        self._human_list = []
+
+    def add_zombie(self, row, col):
+        """
+        Add zombie to the zombie list
+        """
+        self._zombie_list.append((row, col))
+
+    def num_zombies(self):
+        """
+        Return number of zombies
+        """
+        return len(self._zombie_list)
+
+    def zombies(self):
+        """
+        Generator that yields the zombies in the order they were
+        added.
+        """
+        for each_zombie in self._zombie_list:
+            yield each_zombie
+
+    def add_human(self, row, col):
+        """
+        Add human to the human list
+        """
+        self._human_list.append((row, col))
+
+    def num_humans(self):
+        """
+        Return number of humans
+        """
+        return len(self._human_list)
+
+    def humans(self):
+        """
+        Generator that yields the humans in the order they were added.
+        """
+        for each_human in self._human_list:
+            yield each_human
+
+    def compute_distance_field(self, entity_type):
+        """
+        Function computes a 2D distance field
+        Distance at member of entity_queue is zero
+        Shortest paths avoid obstacles and use distance_type distances
+        """
+        pass
+
+    def move_humans(self, zombie_distance):
+        """
+        Function that moves humans away from zombies, diagonal moves
+        are allowed
+        """
+        pass
+
+    def move_zombies(self, human_distance):
+        """
+        Function that moves zombies towards humans, no diagonal moves
+        are allowed
+        """
+        pass
+
+# Start up gui for simulation - You will need to write some code above
+# before this will work without errors
+
+# poc_zombie_gui.run_gui(Zombie(30, 40))
+
