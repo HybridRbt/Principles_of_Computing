609f1fb2b94f6f8f97fa32edf83f650e31edade2
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2015-04-08 16:22:15 -0500

    add tests for intersect function

--- poc_test_wrangler.py
+++ poc_test_wrangler.py
@@ -1,6 +1,35 @@
 import poc_wrangler as pw
 
-list_of_list = ['', 'a', 'bcdef', 'abbbbbbcddddde']
-for index in range(len(list_of_list)):
-    print list_of_list[index]
-    print pw.remove_duplicates(list_of_list[index])
+
+def test1():
+    list_of_list = ['', 'a', 'bcdef', 'abbbbbbcddddde']
+    for index in range(len(list_of_list)):
+        print list_of_list[index]
+        print pw.remove_duplicates(list_of_list[index])
+
+
+def test2():
+    l1 = 'abccde'
+    l2 = 'bcccdeef'
+
+    print str(l1) + '\n' + str(l2)
+    print pw.intersect(l1, l2)
+
+
+def test21():
+    l1 = ''
+    l2 = 'bcccdeef'
+
+    print str(l1) + '\n' + str(l2)
+    print pw.intersect(l1, l2)
+
+
+def test22():
+    l1 = 'a'
+    l2 = 'bcccdeef'
+
+    print str(l1) + '\n' + str(l2)
+    print pw.intersect(l1, l2)
+
+
+test22()
